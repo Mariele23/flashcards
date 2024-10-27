002bfa1d412c262e54790fec1beddee8a568d7d8
Mariele23 <00001115077624SP@al.educacao.sp.gov.br>
2024-10-26 23:00:43 -0300

    fleshcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi a primeira pessoa a viajar no Espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde se localiza Machu Picchu?',
+    'Peru'
+)
+
+criaCartao(
+    'Física',
+    'A que temperatura a água ferve?',
+    '100 ºC'
+)
